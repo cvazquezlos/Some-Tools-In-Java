572bd1438326b279446eff72d2b6998bac1b7207
Carlos Vázquez Losada <c.vazquezlosada@gmail.com>
2016-10-10 21:31:21 +0200

    Algorithm done.

--- Password generator/src/password/generator/PasswordGenerator.java
+++ Password generator/src/password/generator/PasswordGenerator.java
@@ -18,8 +18,8 @@ public class PasswordGenerator {
         this.index = index;
         updateValues(result);
     }
-    
-    private void updateValues(String[] result){
+
+    private void updateValues(String[] result) {
         numChar = Integer.valueOf(result[0]);
         secLevel = result[1];
         specialChar = uploadBooleanValue(result[2]);
@@ -27,46 +27,66 @@ public class PasswordGenerator {
         repeated = uploadBooleanValue(result[4]);
         regionChar = result[5];
     }
-    
-    private Boolean uploadBooleanValue(String result){
-        if (result=="true")
+
+    private Boolean uploadBooleanValue(String result) {
+        if (result == "true") {
             return true;
-        else
+        } else {
             return false;
+        }
     }
 
-    public String createPassword(){
-        String pass="";
+    public String createPassword() {
+        String pass = "";
         String[] elements;
-        for (int i=0; i<numChar; i++){ 
+        for (int i = 0; i < numChar; i++) {
             elements = new String[10];
-            if (numbers && specialChar)
-                for (int j=0; j<elements.length; j++){
-                    if (j>=0 && j<3){
-                        elements[j] = Integer.toString((int) (Math.random()*9));
-                    }
-                    else if (j>=3 && j<6){
+            if (numbers && specialChar) {
+                for (int j = 0; j < elements.length; j++) {
+                    if (j >= 0 && j < 4) {
+                        elements[j] = Integer.toString((int) (Math.random() * 9));
+                    } else if (j >= 4 && j < 6) {
                         char[] valuesAvaible = {'!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '{', '}', '~'};
-                        elements[j] = ""+valuesAvaible[(int) (Math.random() * 27)]+"";
-                    }
-                    else if (j>=6){
+                        elements[j] = "" + valuesAvaible[(int) (Math.random() * 27)] + "";
+                    } else if (j >= 6) {
                         int rnd = (int) (Math.random() * 52);
                         char base = (rnd < 26) ? 'A' : 'a';
-                        elements[j] = ""+(char) (base + rnd % 26)+"";
+                        elements[j] = String.valueOf((char) (base + rnd % 26));
                     }
                 }
-            else if (numbers || specialChar){
-                
-            }
-            else 
-                for (int j=0; j<elements.length; j++){
+            } else if (numbers || specialChar) {
+                if (numbers) {
+                    for (int j = 0; j < elements.length; j++) {
+                        if (j >= 0 && j < 5) {
+                            elements[j] = Integer.toString((int) (Math.random() * 9));
+                        } else if (j >= 5) {
+                            int rnd = (int) (Math.random() * 52);
+                            char base = (rnd < 26) ? 'A' : 'a';
+                            elements[j] = String.valueOf((char) (base + rnd % 26));
+                        }
+                    }
+                } else if (specialChar) {
+                    for (int j = 0; j < elements.length; j++) {
+                        if (j >= 0 && j < 4) {
+                            char[] valuesAvaible = {'!', '"', '#', '$', '%', '&', '(', ')', '*', '+', ',', '-', '.', '/', ':', ';', '<', '=', '>', '?', '@', '[', ']', '^', '{', '}', '~'};
+                            elements[j] = "" + valuesAvaible[(int) (Math.random() * 27)] + "";
+                        } else if (j >= 4) {
+                            int rnd = (int) (Math.random() * 52);
+                            char base = (rnd < 26) ? 'A' : 'a';
+                            elements[j] = String.valueOf((char) (base + rnd % 26));
+                        }
+                    }
+                }
+            } else {
+                for (int j = 0; j < elements.length; j++) {
                     int rnd = (int) (Math.random() * 52);
                     char base = (rnd < 26) ? 'A' : 'a';
                     elements[j] = String.valueOf((char) (base + rnd % 26));
                 }
-            pass+=elements[(int) (Math.random()*9)];
+            }
+            pass += elements[(int) (Math.random() * 9)];
         }
         return pass;
     }
-    
+
 }
